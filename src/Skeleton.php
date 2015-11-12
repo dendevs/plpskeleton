ab2eb0dfa62e157bda72a0423cac815fa7077649
dendev <ddv@awt.be>
2015-11-12 21:45:14 +0100

    add config file

--- src/Skeleton.php
+++ src/Skeleton.php
@@ -1,5 +1,6 @@
 <?php
 namespace DenDev\Plpskeleton;
+require '../vendor/autoload.php';
 use DenDev\Plpadaptability\Adaptability;
 use DenDev\Plpskeleton\SkeletonInterface;
 
@@ -14,14 +15,23 @@ class Skeleton extends Adaptability implements SkeletonInterface
     protected function _set_default_config()
     {
         $root_path = str_replace( 'src', '', dirname( __FILE__ ) );
+        $protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://';
+        $tmp = "$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        $root_url = substr( $tmp, 0, strpos($tmp, 'src/' ) );
         return array( 
             'root_path' => $root_path,
             'log_path' => $root_path . 'logs/',
             'config_path' => $root_path . 'configs/',
             'assets_path' => $root_path . 'assets/',
             'js_path' => $root_path . 'assets/js/',
+            'js_url' => $root_url . 'assets/js/',
             'img_path' => $root_path . 'assets/img/',
             'css_path' => $root_path . 'assets/css/',
          );
     }
 }
+
+//print_r( $_SERVER );
+$object = new Skeleton();
+//print_r( $object->get_config_value( 'js_path' ) );
+print_r( $object->get_config_value( 'js_url' ) );
