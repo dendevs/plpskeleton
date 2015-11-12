61ac36c7db7223315d7f779a24423b8896c085df
dendev <ddv@awt.be>
2015-11-12 22:15:29 +0100

    fix indentation

--- src/Skeleton.php
+++ src/Skeleton.php
@@ -8,19 +8,18 @@ class Skeleton extends Adaptability implements SkeletonInterface
 {
     public function __construct( $krl = false, $config = array() )
     {
-        parent::__construct( $krl, $config );
+        parent::__construct($krl, $config);
     }
 
     protected function _set_default_config()
     {
-        $root_path = str_replace( 'src', '', dirname( __FILE__ ) );
+        $root_path = str_replace('src', '', dirname(__FILE__));
 
         $root_url = '';
-        if( ! empty( $_SERVER['HTTPS'] ) && ! empty( $_SERVER['HTTP_HOST'] ) && ! empty( $_SERVER['REQUEST_URI'] ) )
-        {
+        if(! empty($_SERVER['HTTPS']) && ! empty($_SERVER['HTTP_HOST']) && ! empty($_SERVER['REQUEST_URI']) ) {
             $protocol = ( $_SERVER['HTTPS'] && $_SERVER['HTTPS'] !== 'off' ) ? 'https://' : 'http://';
             $tmp = "$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-            $root_url = substr( $tmp, 0, strpos($tmp, 'src/' ) );
+            $root_url = substr($tmp, 0, strpos($tmp, 'src/'));
         }
         return array( 
             'root_path' => $root_path,
